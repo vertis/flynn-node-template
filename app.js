58f66adf559ec1a3b4f0865cf565911b68b7994e
Luke Chadwick <luke.a.chadwick@gmail.com>
2011-06-03 12:14:41 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+app.configure('development', function () {
+  app.use(express.logger());
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+});
+
+app.configure('production', function () {
+  app.use(express.logger());
+  app.use(express.errorHandler());
+});
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', {layout: true});
+
+app.get('/', function(req, res) {
+  res.render('root');
+});
+
+var port = process.env.PORT || 4000;
+console.log("Listening on " + port);
+
+app.listen(port);
